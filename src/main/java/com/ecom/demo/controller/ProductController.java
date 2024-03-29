32ab3f7ee53ce0868860d77ae3d9aa598b5f2087
varunhegde-trustt <varun.hegde@trustt.com>
2024-03-29 17:54:37 +0530

    product endpoint added

--- src/main/java/com/ecom/demo/controller/ProductController.java
+++ src/main/java/com/ecom/demo/controller/ProductController.java
@@ -1,33 +1,33 @@
-//package com.ecom.demo.controller;
-//
-//import com.ecom.demo.entity.Products;
-//import com.ecom.demo.service.ProductService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//public class ProductController {
-//
-//
-//    @Autowired
-//    private ProductService productService;
-//
-//
-//
-//
-//    @GetMapping("/products")
-//    public List<Products> getAllProducts()
-//    {
-//        return productService.getProduct();
-//    }
-//
+package com.ecom.demo.controller;
+
+import com.ecom.demo.entity.Products;
+import com.ecom.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+
+    @Autowired
+    private ProductService productService;
+
+
+
+
+    @GetMapping("/products")
+    public List<Products> getAllProducts()
+    {
+        return productService.getProduct();
+    }
+
 //    @GetMapping("/product/{category}")
 //    public List<Products> getProductByCategory(@PathVariable int category_id)
 //    {
 //        return productService.getProductByCategory(category_id);
 //    }
-//}
+}
--- src/main/java/com/ecom/demo/service/ProductService.java
+++ src/main/java/com/ecom/demo/service/ProductService.java
@@ -1,24 +1,24 @@
-//package com.ecom.demo.service;
-//
-//import com.ecom.demo.entity.Products;
-//import com.ecom.demo.repository.ProductsRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class ProductService {
-//
-//    @Autowired
-//    private ProductsRepository productsRepository;
-//    public List<Products> getProduct()
-//    {
-//        return productsRepository.findAll();
-//    }
-//
+package com.ecom.demo.service;
+
+import com.ecom.demo.entity.Products;
+import com.ecom.demo.repository.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductsRepository productsRepository;
+    public List<Products> getProduct()
+    {
+        return productsRepository.findAll();
+    }
+
 //    public List<Products> getProductByCategory(int category_id)
 //    {
 //        return productsRepository.findByCategory(category_id);
 //    }
-//}
+}
