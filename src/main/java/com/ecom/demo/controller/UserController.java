55222abd17470aef12b0b8407660676bd454752e
varunhegde-trustt <varun.hegde@trustt.com>
2024-03-29 15:39:00 +0530

    entity final1

---
+++ src/main/java/com/ecom/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.ecom.demo.controller;
+
+import com.ecom.demo.entity.Users;
+import com.ecom.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class UserController {
+
+    //hello
+    @Autowired
+    private UserService userService;
+    // new line
+    @PostMapping("/register")
+    public Users register(@RequestBody Users tempUser){
+        return userService.save(tempUser);
+    }
+}
--- src/main/java/com/ecom/demo/repository/UserRepository.java
+++ src/main/java/com/ecom/demo/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.ecom.demo.repository;
 import com.ecom.demo.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users,Integer> {
+public interface UserRepository extends JpaRepository<Users,Integer> {
 }
